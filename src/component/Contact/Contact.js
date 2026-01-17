232cdaa991cdb0b5026a5ea21d5836c027ce62ea
Hiruni Wijerathna <114440722+HiruniWijerathna@users.noreply.github.com>
2026-01-17 17:54:49 +0530

    add

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
       <section className="contact" id="contact">
 
         <h2 className="heading">
-          Contact <span>Me!</span>
+          Contact <span>Me</span>
         </h2>
 
         <div className="contact-container">

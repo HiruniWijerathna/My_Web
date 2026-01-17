08461a931bf98a003b21418c46d2d382346a8445
Hiruni Wijerathna <114440722+HiruniWijerathna@users.noreply.github.com>
2026-01-18 01:03:17 +0530

    a

--- src/component/Resume/CertificatesEvent/CertificatesEvent.js
+++ src/component/Resume/CertificatesEvent/CertificatesEvent.js
@@ -32,7 +32,7 @@ function CertificatesEvent() {
 
   return (
     <div className="resume-detail certificates active">
-      <h2 className="heading">nMy <span>Certificates & Events</span></h2>
+      <h2 className="heading">My <span>Certificates & Events</span></h2>
       <div className="resume-list2">
         {certificates.map((cert, index) => (
           <div className="resume-item2" key={index}>

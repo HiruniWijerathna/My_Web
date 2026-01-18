ae56347e0f693b047d906b6d7d2aa4350445f5bc
Hiruni Wijerathna <114440722+HiruniWijerathna@users.noreply.github.com>
2026-01-18 17:32:16 +0530

    add

--- src/component/Resume/SideBar/SideBar.js
+++ src/component/Resume/SideBar/SideBar.js
@@ -51,7 +51,7 @@ function SideBar() {
               className={`resume-btn ${activeTab === 'certificates' ? 'active' : ''}`}
               onClick={() => setActiveTab('certificates')}
             >
-              Certificates & Event
+              Achievements
             </button>
             <button
               className={`resume-btn ${activeTab === 'about' ? 'active' : ''}`}
